11b10588b506c6fb819ad99caa1453aca7174162
Levi Wright <lawright379@gmail.com>
2024-09-12 16:56:56 -0300

    building gatsby blog (2/5)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,17 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if(node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
+  }
+}
 
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
